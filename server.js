e26f53378e9213b5fb04cd144523cf3215e86652
Zachary Stark <zach@wagonwheelweb.com>
2020-09-12 11:42:35 -0700

    More basic setup

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
+const dotenv = require('dotenv');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// load values from .env
+dotenv.config();
+
+// express setup
+const app = express();
+app.use(bodyParser.json());
+
+// static setup 
+app.use(express.static('public'));
+
+
+const port = process.env.PORT;
+app.listen(port, () => console.log(`listening on port ${port}...`));
+
+
+
 console.log("hello world");
+
